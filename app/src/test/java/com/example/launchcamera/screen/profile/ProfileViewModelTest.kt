7742210a819e751af42ea02339d28b6131304b70
yeimerrojas <yeimerrojasb@hotmail.com>
2025-09-26 18:43:41 -0500

    Test: Add unit tests for ProfileViewModel
    This commit introduces unit tests for the `ProfileViewModel` to ensure correct behavior when fetching user data.
    
    Key changes:
    
    -   **`ProfileViewModelTest.kt`**:
        -   Added tests to verify the `getUserById` function correctly handles:
            -   Successful data retrieval, ensuring `stateProfile` is `Success` and `userData` is populated.
            -   Errors from the repository, ensuring `stateProfile` is `Error` and `messageError` is set.
            -   Scenarios where the user ID does not exist (returns null data), ensuring `stateProfile` is `Success` and `userData` is null.
            -   Scenarios where the repository returns empty `UserData`, ensuring `stateProfile` is `Success` and `userData` is an empty `UserData` object.
        -   Utilizes `MainCoroutineRule` for testing coroutines.
        -   Mocks `GetUserByIdUseCase` and `SavedStateHandle` to simulate different responses and scenarios.
        -   Verifies that `GetUserByIdUseCase` is invoked with the correct user ID.

---
+++ app/src/test/java/com/example/launchcamera/screen/profile/ProfileViewModelTest.kt
@@ -0,0 +1,121 @@
+package com.example.launchcamera.screen.profile
+
+import androidx.lifecycle.SavedStateHandle
+import com.example.launchcamera.MainCoroutineRule
+import com.example.launchcamera.domain.model.UserData
+import com.example.launchcamera.domain.usescases.GetUserByIdUseCase
+import com.example.launchcamera.mock.TypeMockSimulator
+import com.example.launchcamera.mock.data.getUserDataMockk
+import com.example.launchcamera.screen.profile.viewModel.ProfileViewModel
+import com.example.launchcamera.screen.state.ScreenState
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class ProfileViewModelTest {
+
+    @get:Rule
+    val mainCoroutineRule = MainCoroutineRule()
+
+    @Test
+    fun `When getUserById is called and the id exists in the database then it returns the correct data`() =
+        runTest {
+            // Given
+            val getUserByIdUseCase = providesGetUserByIdUseCase(TypeMockSimulator.SUCCESS)
+            val saveStateHandle = providesSaveStateHandle()
+            val sut = providesSut(getUserByIdUseCase, saveStateHandle)
+            // When
+            sut.getUserById(USER_ID_TEST)
+            // Then
+            coVerify(exactly = 1) { getUserByIdUseCase.invoke(USER_ID_TEST) }
+            assertEquals(ScreenState.Success, sut.stateProfile.value)
+            assertEquals(getUserDataMockk(), sut.userData.value)
+        }
+
+    @Test
+    fun `When getUserById is called and the repository returns an error then it returns a failure result`() =
+        runTest {
+            // Given
+            val getUserByIdUseCase = providesGetUserByIdUseCase(TypeMockSimulator.ERROR)
+            val saveStateHandle = providesSaveStateHandle()
+            val sut = providesSut(getUserByIdUseCase, saveStateHandle)
+            // When
+            sut.getUserById(USER_ID_TEST)
+            // Then
+            coVerify(exactly = 1) { getUserByIdUseCase.invoke(USER_ID_TEST) }
+            assertEquals(ScreenState.Error, sut.stateProfile.value)
+            assertEquals(EXPECTED_EXCEPTION_MESSAGE, sut.messageError.value)
+        }
+
+    @Test
+    fun `When getUserById is called and the user id does not exist then it returns a success result with null data`() =
+        runTest {
+            // Given
+            val getUserByIdUseCase = providesGetUserByIdUseCase(TypeMockSimulator.NULL)
+            val saveStateHandle = providesSaveStateHandle()
+            val sut = providesSut(getUserByIdUseCase, saveStateHandle)
+            // When
+            sut.getUserById(USER_ID_TEST)
+            // Then
+            coVerify(exactly = 1) { getUserByIdUseCase.invoke(USER_ID_TEST) }
+            assertEquals(ScreenState.Success, sut.stateProfile.value)
+            assertEquals(null, sut.userData.value)
+        }
+
+    @Test
+    fun `When getUserById is called and the repository returns empty user data then it returns a success result with empty UserData`() =
+        runTest {
+            // Give
+            val getUserByIdUseCase = providesGetUserByIdUseCase(TypeMockSimulator.EMPTY)
+            val saveStateHandle = providesSaveStateHandle()
+            val sut = providesSut(getUserByIdUseCase, saveStateHandle)
+            // When
+            sut.getUserById(USER_ID_TEST)
+            // Then
+            coVerify(exactly = 1) { getUserByIdUseCase.invoke(USER_ID_TEST) }
+            assertEquals(ScreenState.Success, sut.stateProfile.value)
+            assertEquals(UserData(), sut.userData.value)
+        }
+
+
+    private fun providesSut(
+        getUserByIdUseCase: GetUserByIdUseCase,
+        handle: SavedStateHandle
+    ) = ProfileViewModel(getUserByIdUseCase, handle)
+
+    private fun providesGetUserByIdUseCase(type: TypeMockSimulator) =
+        mockk<GetUserByIdUseCase>().also {
+            when (type) {
+                TypeMockSimulator.SUCCESS -> coEvery {
+                    it.invoke(any())
+                } returns Result.success(
+                    getUserDataMockk()
+                )
+
+                TypeMockSimulator.ERROR -> coEvery { it.invoke(any()) } returns Result.failure(
+                    Exception(EXPECTED_EXCEPTION_MESSAGE)
+                )
+
+                TypeMockSimulator.NULL -> coEvery { it.invoke(any()) } returns Result.success(null)
+                TypeMockSimulator.EMPTY -> coEvery { it.invoke(any()) } returns Result.success(
+                    UserData()
+                )
+            }
+        }
+
+    private fun providesSaveStateHandle() = mockk<SavedStateHandle>().also {
+        every { it.get<String>(any()) } returns USER_ID_TEST
+    }
+
+    companion object {
+        private const val USER_ID_TEST = "1234567"
+        private const val EXPECTED_EXCEPTION_MESSAGE = "Database error"
+    }
+}
